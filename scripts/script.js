90eb9d7bb5dc6c6aa288a0b9edd26c2651c1690b
RobinElseman <143735121+RobinElseman@users.noreply.github.com>
2023-10-04 13:17:49 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 // JavaScript Document
-console.log("hi");
-
 var searchIcon = document.querySelector("header>ul li:first-of-type button");
 var searchIconImg = document.querySelector("header>ul li:first-of-type button img");
 var searchBarButton = document.querySelector("header section:first-of-type button");
@@ -165,120 +163,176 @@ function supportOpenFunction() {
     }
 };
 
-var filterButtonOpen = document.querySelector("body.tees main section:first-of-type button");
 
-if(document.querySelector("body").classList.contains("tees"))
-    {
-
-filterButtonOpen.addEventListener("click", function () { filterOpenFunction() });
-function filterOpenFunction() {
-    document.querySelector("body.tees main section:first-of-type").classList.toggle("filterOpen");
-    if (filterButtonOpen.innerHTML === "Open filters"){
-        filterButtonOpen.innerHTML = "Close filters";}
-        else {
+if (document.querySelector("body").classList.contains("tees")) {
+    var filterButtonOpen = document.querySelector("body.tees main section:first-of-type button");
+    filterButtonOpen.addEventListener("click", function () { filterOpenFunction() });
+    function filterOpenFunction() {
+        if (filterButtonOpen.innerHTML === "Open filters") {
+            filterButtonOpen.innerHTML = "Close filters";
+            filterButtonOpen.ariaExpanded = "true";
+            document.querySelector("body.tees main section:nth-of-type(2)").ariaHidden = false;
+        } else {
             filterButtonOpen.innerHTML = "Open filters";
+            document.querySelector("body.tees main section:nth-of-type(2)").ariaHidden = true;
+            filterButtonOpen.ariaExpanded = "false";
         }
+    };
+
+    var sortByButton1 = document.querySelector("body.tees main section:nth-of-type(2) form ul li:first-of-type button");
+    sortByButton1.addEventListener("click", function () { sortByFunction1() });
+    function sortByFunction1() {
+        document.querySelector("body.tees main section:nth-of-type(2) form ul li:first-of-type button img").classList.toggle("closedLiArrow");
+        if (sortByButton1.ariaExpanded == "false") {
+            sortByButton1.ariaExpanded = "true";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:first-of-type div").ariaHidden = "false";
+        }
+        else {
+            sortByButton1.ariaExpanded = "false";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:first-of-type div").ariaHidden = "true";
+        }
+    };
+
+    var sortByButton2 = document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(2) button");
+    sortByButton2.addEventListener("click", function () { sortByFunction2() });
+    function sortByFunction2() {
+        document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(2) button img").classList.toggle("closedLiArrow");
+        if (sortByButton2.ariaExpanded == "false") {
+            sortByButton2.ariaExpanded = "true";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(2) div").ariaHidden = "false";
+        }
+        else {
+            sortByButton2.ariaExpanded = "false";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(2) div").ariaHidden = "true";
+        }
+    };
+
+    var sortByButton3 = document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(3) button");
+    sortByButton3.addEventListener("click", function () { sortByFunction3() });
+    function sortByFunction3() {
+        document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(3) button img").classList.toggle("closedLiArrow");
+        if (sortByButton3.ariaExpanded == "false") {
+            sortByButton3.ariaExpanded = "true";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(3) div").ariaHidden = "false";
+        }
+        else {
+            sortByButton3.ariaExpanded = "false";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(3) div").ariaHidden = "true";
+        }    };
+
+    var sortByButton4 = document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(4) button");
+    sortByButton4.addEventListener("click", function () { sortByFunction4() });
+    function sortByFunction4() {
+        document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(4) button img").classList.toggle("closedLiArrow");
+        if (sortByButton4.ariaExpanded == "false") {
+            sortByButton4.ariaExpanded = "true";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(4) div").ariaHidden = "false";
+        }
+        else {
+            sortByButton4.ariaExpanded = "false";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(4) div").ariaHidden = "true";
+        }  };
+
+    var sortByButton5 = document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(5) button");
+    sortByButton5.addEventListener("click", function () { sortByFunction5() });
+    function sortByFunction5() {
+        document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(5) button img").classList.toggle("closedLiArrow");
+        if (sortByButton5.ariaExpanded == "false") {
+            sortByButton5.ariaExpanded = "true";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(5) div").ariaHidden = "false";
+        }
+        else {
+            sortByButton5.ariaExpanded = "false";
+            document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(5) div").ariaHidden = "true";
+        }  };
 };
 
-var sortByButton1 = document.querySelector("body.tees main section:nth-of-type(2) form ul button:first-of-type");
-sortByButton1.addEventListener("click", function () { sortByFunction1() });
-function sortByFunction1() {
-    document.querySelector("body.tees main section:nth-of-type(2) form ul li:first-of-type").classList.toggle("closedLi");
-    document.querySelector("body.tees main section:nth-of-type(2) form ul button:first-of-type img").classList.toggle("closedLiArrow");
-};
-
-var sortByButton2 = document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(2)");
-sortByButton2.addEventListener("click", function () { sortByFunction2() });
-function sortByFunction2() {
-    document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(2)").classList.toggle("closedLi");
-    document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(2) img").classList.toggle("closedLiArrow");
-};
-
-var sortByButton3 = document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(3)");
-sortByButton3.addEventListener("click", function () { sortByFunction3() });
-function sortByFunction3() {
-    document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(3)").classList.toggle("closedLi");
-    document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(3) img").classList.toggle("closedLiArrow");
-};
-
-var sortByButton4 = document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(4)");
-sortByButton4.addEventListener("click", function () { sortByFunction4() });
-function sortByFunction4() {
-    document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(4)").classList.toggle("closedLi");
-    document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(4) img").classList.toggle("closedLiArrow");
-};
-
-var sortByButton5 = document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(5)");
-sortByButton5.addEventListener("click", function () { sortByFunction5() });
-function sortByFunction5() {
-    document.querySelector("body.tees main section:nth-of-type(2) form ul li:nth-of-type(5)").classList.toggle("closedLi");
-    document.querySelector("body.tees main section:nth-of-type(2) form ul button:nth-of-type(5) img").classList.toggle("closedLiArrow");
-};
-
-var filterButtonMain = document.querySelector(".tees main section:first-of-type button");
-filterButtonMain.addEventListener("click", function () { filterOpenClose() });
-function filterOpenClose() {
-    document.querySelector("body.tees main section:nth-of-type(2)").classList.toggle("closedFilterMenu");
-};
-};
 
 
 
-
-if(document.querySelector("body").classList.contains("shirt"))
-    {
+if (document.querySelector("body").classList.contains("shirt")) {
 
 
-var imageShirtPreview = document.querySelector(".shirt main section:first-of-type img");
-var imageFront = document.querySelector(".shirt main section:first-of-type button:first-of-type");
-var imageBack = document.querySelector(".shirt main section:first-of-type button:nth-of-type(2)");
+    var imageShirtPreview = document.querySelector(".shirt main section:first-of-type img");
+    var imageFront = document.querySelector(".shirt main section:first-of-type button:first-of-type");
+    var imageBack = document.querySelector(".shirt main section:first-of-type button:nth-of-type(2)");
 
-imageFront.addEventListener("click", function () {
-    imageShirtPreview.src = "images/shirt.webp";
-    imageFront.classList.add("activeShirt");
-    imageFront.classList.remove("inactiveShirt");
-    imageBack.classList.add("inactiveShirt");
-    imageBack.classList.remove("activeShirt");
-});
+    imageFront.addEventListener("click", function () {
+        imageShirtPreview.src = "images/shirt.webp";
+        imageFront.classList.add("activeShirt");
+        imageFront.classList.remove("inactiveShirt");
+        imageBack.classList.add("inactiveShirt");
+        imageBack.classList.remove("activeShirt");
+    });
 
-imageBack.addEventListener("click", function () {
-    imageShirtPreview.src = "images/shirt6.webp";
-    imageBack.classList.add("activeShirt");
-    imageBack.classList.remove("inactiveShirt");
-    imageFront.classList.add("inactiveShirt");
-    imageFront.classList.remove("activeShirt");
-});
+    imageBack.addEventListener("click", function () {
+        imageShirtPreview.src = "images/shirt6.webp";
+        imageBack.classList.add("activeShirt");
+        imageBack.classList.remove("inactiveShirt");
+        imageFront.classList.add("inactiveShirt");
+        imageFront.classList.remove("activeShirt");
+    });
 
-var infoButton1 = document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>button:first-of-type");
-infoButton1.addEventListener("click", function () { infoFunction1() });
-function infoFunction1 () {
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type").classList.toggle("activeInfo");
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) button:first-of-type").classList.toggle("activeInfoText");
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) button:first-of-type img").classList.toggle("activeInfoImg");
-}
+    var infoButton1 = document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) li:first-of-type button");
+    infoButton1.addEventListener("click", function () { infoFunction1() });
+    function infoFunction1() {
+        document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type button img").classList.toggle("activeInfoImg");
+        if(document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type button").ariaExpanded == "false"){
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type button").ariaExpanded = "true";
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type button").classList.add("activeInfoText");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type div").classList.add("activeInfo");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type div").ariaHidden = "false";
+        }
+        else {
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type button").ariaExpanded = "false";
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type button").classList.remove("activeInfoText");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type div").classList.remove("activeInfo");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:first-of-type div").ariaHidden = "true";
+        }
+    };
 
-var infoButton2 = document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>button:nth-of-type(2)");
-infoButton2.addEventListener("click", function () { infoFunction2() });
-function infoFunction2 () {
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2)").classList.toggle("activeInfo");
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) button:nth-of-type(2)").classList.toggle("activeInfoText");
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) button:nth-of-type(2) img").classList.toggle("activeInfoImg");
-}
+    var infoButton2 = document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) li:nth-of-type(2) button");
+    infoButton2.addEventListener("click", function () { infoFunction2() });
+    function infoFunction2() {
+        document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) button img").classList.toggle("activeInfoImg");
+        if(document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) button").ariaExpanded == "false"){
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) button").ariaExpanded = "true";
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) button").classList.add("activeInfoText");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) div").classList.add("activeInfo");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) div").ariaHidden = "false";
+        }
+        else {
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) button").ariaExpanded = "false";
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) button").classList.remove("activeInfoText");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) div").classList.remove("activeInfo");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(2) div").ariaHidden = "true";
+        }
+    }
 
-var infoButton3 = document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>button:nth-of-type(3)");
-infoButton3.addEventListener("click", function () { infoFunction3() });
-function infoFunction3 () {
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3)").classList.toggle("activeInfo");
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) button:nth-of-type(3)").classList.toggle("activeInfoText");
-    document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) button:nth-of-type(3) img").classList.toggle("activeInfoImg");
-}
+    var infoButton3 = document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2) li:nth-of-type(3) button");
+    infoButton3.addEventListener("click", function () { infoFunction3() });
+    function infoFunction3() {
+        document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) button img").classList.toggle("activeInfoImg");
+        if(document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) button").ariaExpanded == "false"){
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) button").ariaExpanded = "true";
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) button").classList.add("activeInfoText");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) div").classList.add("activeInfo");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) div").ariaHidden = "false";
+        }
+        else {
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) button").ariaExpanded = "false";
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) button").classList.remove("activeInfoText");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) div").classList.remove("activeInfo");
+            document.querySelector(".shirt main section:nth-of-type(2)>ul:nth-of-type(2)>li:nth-of-type(3) div").ariaHidden = "true";
+        }
+    }
 
-var addToCartButton = document.querySelector(".shirt main section:nth-of-type(3) button:first-of-type");
-var cartImg = document.querySelector("header ul:first-of-type li:nth-of-type(2) button img");
-addToCartButton.addEventListener("click", function () { addToCartFunction() });
-function addToCartFunction () {
-    cartImg.classList.add("activeCart");
-    
-}
-cartImg.addEventListener("animationend", function () { cartImg.classList.remove("activeCart") });
+    var addToCartButton = document.querySelector(".shirt main section:nth-of-type(3) button:first-of-type");
+    var cartImg = document.querySelector("header ul:first-of-type li:nth-of-type(2) button img");
+    addToCartButton.addEventListener("click", function () { addToCartFunction() });
+    function addToCartFunction() {
+        cartImg.classList.add("activeCart");
+
+    }
+    cartImg.addEventListener("animationend", function () { cartImg.classList.remove("activeCart") });
 };
